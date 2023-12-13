8bb6b4c394d7c39591550004b9fc0d740f96fbd6
SophieO1970 <sophieolondie@gmail.com>
2023-12-13 12:01:57 -0800

    add a user_input file to show the output of success and failure

---
+++ user_input.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+void get_input_command(char *command, size_t size)
+{
+        if (fgets(command, size, stdin) == NULL)
+        {
+                if (feof(stdin))
+                {
+                        kide_print("\n");
+                        exit(EXIT_SUCCESS);
+                }
+                else
+                {
+                        kide_print("Error while reading input.\n");
+                        exit(EXIT_FAILURE);
+                }
+        }
+        command[strcspn(command, "\n")] = '\0';
+}
