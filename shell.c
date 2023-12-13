bd5628f2722ecb311d85ec041473ec6af57d8856
SophieO1970 <sophieolondie@gmail.com>
2023-12-13 11:58:52 -0800

    add a shell.c file with the execute command

---
+++ shell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+int main(void)
+{
+        char command[100];
+
+        while (1)
+        {
+                display_prompt();
+                get_input_command(command, sizeof(command));
+                execute_command(command);
+        }
+        return 0;
+}
+
